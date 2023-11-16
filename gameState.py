98f27c607d884749a77f2ed9544fe625de34aad6
georgemusson79 <149239702+georgemusson79@users.noreply.github.com>
2023-11-16 00:43:32 +0000

    Added gameState.py

---
+++ gameState.py
@@ -0,0 +1,16 @@
+from enum import Enum
+class GameStates(Enum):
+    MAIN_MENU=1
+    MAIN_GAME=2
+global gameState
+gameState=None
+
+def setGameState(state):
+    #used to indicate which part of the code is running, holds a value from the GameStates enum
+    global gameState
+    gameState=state
+
+def getGameState():
+    #used to indicate which part of the code is running, holds a value from the GameStates enum
+    global gameState
+    return gameState
